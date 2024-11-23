52781411a12ea2234ff741745337992056b0ace7
CodeArtist <codeartist.net@gmail.com>
2024-11-23 22:54:09 +0100

    client app swapped from angular to react

--- aps/client/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { InvoicesComponent } from './invoices/invoices.component';
-import { ReceiversComponent } from './receivers/receivers.component';
-
-export const routes: Routes = [
-  { path: 'invoices', component: InvoicesComponent },
-  { path: 'receivers', component: ReceiversComponent },
-];
